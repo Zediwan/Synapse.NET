2ef9eb0ce0da2684f39ca1408a6b95a962e4d776
Jeremy Moser <jeremymoser@opacc.ch>
2025-05-26 11:37:24 +0200

    Created first tests for initial model structures

--- Synapse.NET.Tests/Models/ConnectionTest.cs
+++ Synapse.NET.Tests/Models/ConnectionTest.cs
@@ -1,6 +1,48 @@
-﻿namespace Synapse.NET.Tests.Models;
+﻿using Synapse.NET.Helpers;
+using Synapse.NET.Models;
+
+namespace Synapse.NET.Tests.Models;
 
 public class ConnectionTest
 {
-    
+    #region Constructor
+
+    [Fact]
+    public void Connection_Constructor_ShouldInitializeProperties()
+    {
+        // Arrange
+        var fromNode = new Node(bias: 0.0) { Value = 1.0 };
+        var toNode = new Node(bias: 0.0);
+        double weight = 2.0;
+        var activationFunction = ActivationFunctions.Sigmoid;
+        // Act
+        var connection = new Connection(fromNode, toNode, weight, activationFunction);
+        // Assert
+        Assert.Equal(fromNode, connection.From);
+        Assert.Equal(toNode, connection.To);
+        Assert.Equal(weight, connection.Weight);
+        Assert.Equal(activationFunction, connection.ActivationFunction);
+    }
+
+    #endregion
+
+    #region Value
+
+    [Theory]
+    [InlineData(1.0, 2.0, 2.0)]
+    [InlineData(-1.0, 2.0, -2.0)]
+    [InlineData(0.0, 2.0, 0.0)]
+    public void Connection_Value_ShouldBeCalculatedCorrectly(double weight, double fromValue, double expectedValue)
+    {
+        // Arrange
+        var fromNode = new Node(bias: 0.0) { Value = fromValue };
+        var toNode = new Node(bias: 0.0);
+        var connection = new Connection(fromNode, toNode, weight, ActivationFunctions.Linear);
+        // Act
+        var actualValue = connection.Value;
+        // Assert
+        Assert.Equal(expectedValue, actualValue);
+    }
+
+    #endregion
 }
--- Synapse.NET.Tests/Models/NodeTest.cs
+++ Synapse.NET.Tests/Models/NodeTest.cs
@@ -1,6 +1,88 @@
-﻿namespace Synapse.NET.Tests.Models;
+﻿using Synapse.NET.Helpers;
+using Synapse.NET.Models;
+
+namespace Synapse.NET.Tests.Models;
 
 public class NodeTest
 {
-    
+    #region Constructor
+
+    [Fact]
+    public void Node_InitialValue_ShouldBeZero()
+    {
+        // Act
+        var node = new Node();
+        // Assert
+        Assert.Equal(0, node.Value);
+        Assert.Equal(0, node.Bias);
+    }
+
+    [Fact]
+    public void Node_SetValue_ShouldUpdateValue()
+    {
+        // Act
+        var node = new Node
+        {
+            Value = 5.0
+        };
+        // Assert
+        Assert.Equal(5.0, node.Value);
+        Assert.Equal(0, node.Bias);
+    }
+
+    [Fact]
+    public void Node_SetBias_ShouldUpdateBias()
+    {
+        // Act
+        var node = new Node(bias: 2.0);
+        // Assert
+        Assert.Equal(2.0, node.Bias);
+    }
+
+    #endregion
+
+    #region Value
+
+    #region Bias
+
+    [Theory]
+    [InlineData(0.0)]
+    [InlineData(1.0)]
+    [InlineData(-1.0)]
+    public void Node_GetValue_ShouldAddBias(int bias)
+    {
+        // Arrange
+        const double value = 3.0;
+        var expectedValue = value + bias;
+        // Act
+        var node = new Node(bias: bias)
+        {
+            Value = value
+        };
+        // Assert
+        Assert.Equal(expectedValue, node.Value);
+    }
+
+    #endregion
+
+    #region Connection
+
+    // Create a test with mocked connections
+    [Fact]
+    public void Node_GetValue_ShouldSumInConnections()
+    {
+        // Arrange
+        var node = new Node(bias: 0.0);
+        var connection1 = new Connection(new Node(bias:0.0) { Value = 1.0 }, node, 1.0, ActivationFunctions.Linear);
+        var connection2 = new Connection(new Node(bias: 0.0) { Value = 1.0 }, node, 1.0, ActivationFunctions.Linear);
+        var expectedValue = connection1.Value + connection2.Value;
+
+        // Assert
+        Assert.Equal(expectedValue, node.Value);
+    }
+
+    #endregion
+
+    #endregion
+
 }
