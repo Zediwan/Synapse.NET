fe7c616a2fba41834756b6468b1195879a99f06d
Jeremy Moser <jeremymoser@opacc.ch>
2025-05-27 09:26:25 +0200

    Created a static method to create a connection between two Nodes. Created a static method in Connection.cs to see if two Nodes are connected.

--- Synapse.NET/Models/Connection.cs
+++ Synapse.NET/Models/Connection.cs
@@ -19,4 +19,32 @@ public class Connection
         From.OutConnections.Add(this);
         To.InConnections.Add(this);
     }
+
+    /// <summary>
+    /// Creates a connection between two nodes with the specified weight.
+    /// </summary>
+    /// <param name="from"></param>
+    /// <param name="to"></param>
+    /// <param name="weight"></param>
+    /// <returns></returns>
+    public static Connection Connect(Node from, Node to, double weight = 1.0)
+    {
+        ArgumentNullException.ThrowIfNull(from);
+        ArgumentNullException.ThrowIfNull(to);
+
+        return new Connection(from, to, weight);
+    }
+
+    /// <summary>
+    /// Checks if two nodes are connected, either directly or indirectly.
+    /// </summary>
+    /// <param name="from"></param>
+    /// <param name="to"></param>
+    /// <returns></returns>
+    public static bool AreConnected(Node from, Node to)
+    {
+        ArgumentNullException.ThrowIfNull(from);
+        ArgumentNullException.ThrowIfNull(to);
+        return from.OutConnections.Any(c => c.To == to) || to.InConnections.Any(c => c.From == from);
+    }
 }
